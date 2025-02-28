b582971f3bc68507138db601a1efa59ad627cf16
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2025-02-28 20:15:11 -0300

    Update Loja-online.java

--- Loja online em Java/Loja-online.java
+++ Loja online em Java/Loja-online.java
@@ -1,14 +1,14 @@
-package lojaonline;
+package br.com.henriquebg.lojaonline;
 
-public class Lojaonline
+public class LojaOnline 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		Cliente cliente = new Cliente("Fulano", "fulano@email.com", "400.289.220-00");
-	
-		Produto livro = new Produto("O Senhor dos Anéis", 100.0, 0.5);
-		Produto jogo = new Produto("Minecraft", 250.0, 0.5);
-		Produto console = new Produto("PlayStation 5", 2500.0, 3.0);
+		Cliente cliente = new Cliente("Fulano", "fulano@email.com", "00000000000");
+		
+		Produto livro = new Produto("O Senhor dos Aneis", 50.0);
+		Produto jogo = new Produto("GTA 6", 250.0);
+		Produto console = new Produto("Nintendo Switch", 2500.0);
 		
 		Pedido pedido = new Pedido(cliente);
 		pedido.adicionarProduto(livro);
