a3e295a6159eca32e962b08fd9335be6ee2663ff
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2025-02-28 22:13:16 -0300

    Update Loja-online.java

--- Loja online em Java/Loja-online.java
+++ Loja online em Java/Loja-online.java
@@ -1,19 +1,23 @@
 package br.com.henriquebg.lojaonline;
 
-public class LojaOnline 
+public class LojaVirtual 
 {
 	public static void main(String[] args) 
 	{
-		Cliente cliente = new Cliente("Fulano", "fulano@email.com", "00000000000");
+		Cliente cliente = new Cliente("Fulano", "fulano@email.com");
 		
-		Produto livro = new Produto("O Senhor dos Aneis", 50.0);
-		Produto jogo = new Produto("GTA 6", 250.0);
-		Produto console = new Produto("Nintendo Switch", 2500.0);
+		Produto livro = new LivroFisico("O Senhor dos Anéis", 50.0, 0.5, "HarperCollins", "8595086354");
+		Produto jogo = new JogoDigital("Mario Odyssey", 250.0, 10000.0, "Nintendo");
+		Produto console = new Console("Nintendo Switch", 2500.0, 2.0, "Nintendo");
+		Produto filme = new FilmeDigital("Piratas do Caribe", 25.0, 500.0, "Warner Bros.");
 		
 		Pedido pedido = new Pedido(cliente);
+		Cupom cupomDesconto = new Cupom("CARNAVAL20", 0.2);
+		pedido.aplicarDesconto(cupomDesconto);
 		pedido.adicionarProduto(livro);
 		pedido.adicionarProduto(jogo);
 		pedido.adicionarProduto(console);
+		pedido.adicionarProduto(filme);
 		pedido.finalizar();
 	}
 }
