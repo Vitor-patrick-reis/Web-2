21c8d6da456424fa5e478dc435cc033124cfa80e
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2025-04-04 15:34:03 -0300

    Delete Tarefas/Tarefa_2 directory

--- Tarefas/Tarefa_2/script.js
+++
@@ -1,8 +0,0 @@
-function addEmailField() {
-    const container = document.getElementById("email-container");
-    const input = document.createElement("input");
-    input.type = "email";
-    input.name = "emails[]";
-    input.required = true;
-    container.appendChild(input);
-}
